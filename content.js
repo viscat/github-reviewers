fb123239fdffd020a9dbc5c5060795744eb2d8cf
Gerard Costa <viscat88@gmail.com>
2018-04-30 02:30:44 +0200

    Remove console logs and reformat code

--- content.js
+++ content.js
@@ -4,28 +4,26 @@ button.innerHTML = "Add reviewers";
 button.type = "button";
 button.classList.add("discussion-sidebar-heading");
 button.classList.add("discussion-sidebar-toggle");
-button.onclick = function(){
-	chrome.storage.sync.get('reviewerIds', function(data) {
-		let reviewerSelectors = "";
-		let code = "";
-		data.reviewerIds.forEach(function(id){
-			reviewerSelectors += "input[value='"+id+"']:not(:checked),"
-		});
-		if (data.reviewerIds.length === 0) {
-			alert("No reviewers configured");
-			return;
-		}
-		if (!document.getElementById('review-filter-field').parentNode.parentNode.parentNode.parentNode.classList.contains('js-active-navigation-container')) {
-			document.querySelectorAll("button[aria-label='Request a review']")[0].click();
-		}
-		setTimeout(function(){ 
-			document.querySelectorAll(reviewerSelectors.slice(0,-1)).forEach(function(e){
-				e.click()
-			}); 
-			console.log('b');
-			document.querySelectorAll("button[aria-label='Request a review']")[0].click();
-			console.log('c');
-		}, 500);
-	});
+button.onclick = function () {
+    chrome.storage.sync.get('reviewerIds', function (data) {
+        let reviewerSelectors = "";
+        data.reviewerIds.forEach(function (id) {
+            reviewerSelectors += "input[value='" + id + "']:not(:checked),"
+        });
+        if (data.reviewerIds.length === 0) {
+            alert("No reviewers configured");
+            return;
+        }
+        let reviewersBox = document.getElementById('review-filter-field').parentNode.parentNode.parentNode.parentNode;
+        if (!reviewersBox.classList.contains('js-active-navigation-container')) {
+            document.querySelectorAll("button[aria-label='Request a review']")[0].click();
+        }
+        setTimeout(function () {
+            document.querySelectorAll(reviewerSelectors.slice(0, -1)).forEach(function (e) {
+                e.click()
+            });
+            document.querySelectorAll("button[aria-label='Request a review']")[0].click();
+        }, 500);
+    });
 };
 element.parentNode.insertBefore(button, element.nextSibling);
