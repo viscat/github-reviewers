fa651d6765ff0319998983101c26da6a9fef0f56
Gerard Costa <viscat88@gmail.com>
2018-05-01 18:37:15 +0200

    Add copy&paste reviewers feature and other improvements

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ GitHub Reviewers (Chrome Extension)
 
 Add all reviewers to your GitHub PR in one click.
 
-![Instructions](https://raw.githubusercontent.com/viscat/github-reviewers/master/images/instructions.png)
+## Instructions
 
-Limitations:
-  - It only allow to add users by their numeric id
-  - The 'Add Reviewrs' link only appears when page is fully loaded (doesn't load on SPA, you need to reload the page)
+- Click on "Copy reviewers" to copy the list of current reviewers.
+- Click on "Paste reviewers" to add copied reviewers to the PR.
--- content.js
+++ content.js
@@ -1,29 +1,73 @@
-let button = document.createElement("button");
-let element = document.querySelectorAll("button[aria-label='Request a review']")[0];
-button.innerHTML = "Add reviewers";
-button.type = "button";
-button.classList.add("discussion-sidebar-heading");
-button.classList.add("discussion-sidebar-toggle");
-button.onclick = function () {
-    chrome.storage.sync.get('reviewerIds', function (data) {
-        let reviewerSelectors = "";
-        data.reviewerIds.forEach(function (id) {
-            reviewerSelectors += "input[value='" + id + "']:not(:checked),"
+document.addEventListener('pjax:end', function (event) {
+    loadUI();
+});
+loadUI();
+
+function loadUI() {
+    let element = document.querySelectorAll("button[aria-label='Request a review']")[0];
+    if (typeof element === 'undefined') {
+        return;
+    }
+
+    if (!document.getElementById('copy-reviewers')) {
+        element.parentNode.insertBefore(createCopyReviewersButton(), element.nextSibling);
+    }
+    if (!document.getElementById('paste-reviewers')) {
+        element.parentNode.insertBefore(createPasteReviewersButton(), element.nextSibling);
+    }
+}
+
+function createCopyReviewersButton() {
+    let button = document.createElement("button");
+    button.id = "copy-reviewers";
+    button.innerHTML = "Copy reviewers";
+    button.type = "button";
+    button.classList.add("discussion-sidebar-heading");
+    button.classList.add("discussion-sidebar-toggle");
+
+    button.onclick = function () {
+        let reviewerIds = [];
+        document.querySelectorAll('.js-hovercard-left').forEach(function (reviewer) {
+            reviewerIds.push(reviewer.getAttribute('data-hovercard-user-id'));
         });
-        if (data.reviewerIds.length === 0) {
-            alert("No reviewers configured");
-            return;
-        }
-        let reviewersBox = document.getElementById('review-filter-field').parentNode.parentNode.parentNode.parentNode;
-        if (!reviewersBox.classList.contains('js-active-navigation-container')) {
-            document.querySelectorAll("button[aria-label='Request a review']")[0].click();
-        }
-        setTimeout(function () {
-            document.querySelectorAll(reviewerSelectors.slice(0, -1)).forEach(function (e) {
-                e.click()
+        chrome.storage.sync.set({reviewerIds: reviewerIds});
+    };
+
+    return button;
+}
+
+function createPasteReviewersButton() {
+    let button = document.createElement("button");
+    button.innerHTML = "Paste reviewers";
+    button.type = "button";
+    button.id = "paste-reviewers";
+    button.classList.add("discussion-sidebar-heading");
+    button.classList.add("discussion-sidebar-toggle");
+
+    button.onclick = function () {
+        chrome.storage.sync.get('reviewerIds', function (data) {
+            let reviewerSelectors = "";
+            data.reviewerIds.forEach(function (id) {
+                reviewerSelectors += "input[value='" + id + "']:not(:checked),"
             });
-            document.querySelectorAll("button[aria-label='Request a review']")[0].click();
-        }, 500);
-    });
-};
-element.parentNode.insertBefore(button, element.nextSibling);
+            if (data.reviewerIds.length === 0) {
+                alert("No reviewers configured");
+                return;
+            }
+            let reviewersBox = document.getElementById('review-filter-field').parentNode.parentNode.parentNode.parentNode;
+            if (!reviewersBox.classList.contains('js-active-navigation-container')) {
+                document.querySelectorAll("button[aria-label='Request a review']")[0].click();
+            }
+            setTimeout(function () {
+                document.querySelectorAll(reviewerSelectors.slice(0, -1)).forEach(function (e) {
+                    e.click()
+                });
+                document.querySelectorAll("button[aria-label='Request a review']")[0].click();
+                setTimeout(function () {
+                    loadUI();
+                }, 500);
+            }, 500);
+        });
+    };
+    return button;
+}
